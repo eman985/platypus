c2b6d6b873d23a9ea71a1d66693fe06fa6ced20f
Sara Roselli <120966748+sararoselli@users.noreply.github.com>
2025-07-27 16:31:10 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendMessage: (message) => ipcRenderer.invoke('gemini-chat', message)
-});
-
